5db26fefd6d3366aa1ee061c14806a815343598e
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2023-09-25 14:24:29 +0200

    download

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,20 +1,16 @@
 window.jsPDF = window.jspdf.jsPDF;
-const { jsPDF } = require('jspdf');
 
 const downloadPDF = () => {
     var doc = new jsPDF();
 
-    var elementHTML = document.querySelector("body");
+    var elementHTML = document.getElementById("resume");
 
     doc.html(elementHTML, {
-        callback: function (doc) {
-            doc.save('curriculum-vitae.pdf');
-        },
-        margin: [10, 10, 10, 10],
-        autoPaging: 'text',
+        callback: doc => doc.save('curriculum-vitae.pdf'),
+        margin: [15, 15, 15, 15],
         x: 0,
         y: 0,
-        width: 1240,
+        width: 180,
         windowWidth: 1240
     });
 }
